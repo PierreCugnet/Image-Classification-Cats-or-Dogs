c57e0ce6ee6fd79213dd177177c7101007261acb
pierrecugnet <pierre.cugnet@hei.yncrea.fr>
2020-10-13 11:03:29 +0200

    update

--- cat_dog_vgg16.py
+++ cat_dog_vgg16.py
@@ -11,11 +11,11 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.metrics import confusion_matrix, plot_confusion_matrix
+from tensorflow.keras.models import model_from_json
 import numpy as np
-import pickle
-train_path = '/kaggle/input/cat-and-dog/training_set/training_set'
+train_path = 'kaggle/input/training_set/training_set'
 # valid_path = 'data/dogs-vs-cats/valid'
-test_path = '../input/cat-and-dog/test_set/test_set'
+test_path = 'kaggle/input/test_set/test_set'
 
 train_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
     .flow_from_directory(directory=train_path, target_size=(224,224), classes=['cats', 'dogs'], batch_size=10)
@@ -23,7 +23,7 @@ test_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.v
     .flow_from_directory(directory=test_path, target_size=(224,224), classes=['cats', 'dogs'], batch_size=10, shuffle=False)
     
 vgg16_model = tf.keras.applications.vgg16.VGG16()
-vgg16_model.summary()
+
 
 model = Sequential()
 for layer in vgg16_model.layers[:-1]:
@@ -31,24 +31,33 @@ for layer in vgg16_model.layers[:-1]:
     
 for layer in model.layers:
     layer.trainable = False
+    
 model.add(Dense(units=2, activation='softmax'))
-
+model.summary()
 model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
-early_stopping = EarlyStopping(monitor='val_loss', patience=5, verbose=1, mode='auto')  
+#early_stopping = EarlyStopping(monitor='val_loss', patience=2, verbose=1, mode='auto')  
 history = model.fit(x = train_batches, 
           steps_per_epoch = len(train_batches),
-          epochs = 5,
-          verbose = 2,
-          callbacks=[early_stopping]
-         )
+          epochs = 1)
 
 
 
 
-predictions = model.predict(x = test_batches, steps = len(test_batches), verbose = 0)
+predictions = model.predict(x = test_batches, steps = len(test_batches))
 cm = confusion_matrix(y_true=test_batches.classes, y_pred=np.argmax(predictions, axis=-1))
 cm_plot_labels = ['cat','dog']
-plot_confusion_matrix(cm=cm, classes=cm_plot_labels, title='Confusion Matrix')
 
-model.save("model.h5")
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+
+model.save_weights("model.h5")
+
+#Load the model
+
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
